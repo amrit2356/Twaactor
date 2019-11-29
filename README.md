6ee7939423ea606089d5988013be83e9f4ef50ac
Pandey <amritanshu.pandey@intel.com>
2019-11-30 00:50:29 +0530

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Twaactor
 Extract Twitter Data in real time - Heritage of Falcon in Qatar
-This project belongs to TEAM ARIS(Advanced Robotics Intelligent Systems)--> use it at your own discretion ;P
+This project belongs to TEAM ARIS(Advanced Robotics Intelligent Systems)--> use it at your own discretion 
+;P
---
+++ tweet_extractor.py
@@ -0,0 +1,41 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+import pandas as pd
+import csv
+import re #regular expression
+from textblob import TextBlob
+import string
+import preprocessor as p
+
+#pass twitter credentials to tweepy
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+
+def clean_tweets(tweet):
+ 
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(tweet)
+#after tweepy preprocessing the colon symbol left remain after      #removing mentions
+    tweet = re.sub(r':', '', tweet)
+    tweet = re.sub(r'‚Ä¶', '', tweet)
+#replace consecutive non-ASCII characters with a space
+    tweet = re.sub(r'[^\x00-\x7F]+',' ', tweet)
+#remove emojis from tweet
+    tweet = emoji_pattern.sub(r'', tweet)
+#filter using NLTK library append it to a string
+    filtered_tweet = [w for w in word_tokens if not w in stop_words]
+    filtered_tweet = []
+#looping through conditions
+    for w in word_tokens:
+#check tokens against stop words , emoticons and punctuations
+        if w not in stop_words and w not in emoticons and w not in string.punctuation:
+            filtered_tweet.append(w)
+    return ' '.join(filtered_tweet)
+    #print(word_tokens)
+    #print(filtered_sentence)return tweet
+
+
+clean_text = p.clean(twitter_text)
