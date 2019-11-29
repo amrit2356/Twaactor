562fd4e806aa2e317686b6ac29af240360775b14
Pandey <amritanshu.pandey@intel.com>
2019-11-30 01:18:05 +0530

    creating tweet extractor class

--- tweet_extractor.py
+++ tweet_extractor.py
@@ -1,3 +1,18 @@
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+
+import twitter_credentials
+
+class TwitterStreamer():
+    def stream_tweets(self, fetched tweets_filename, hash tag list):
+        pass 
+    
+    def on_data(self,data):
+        print(data)
+        return true
+
+    def off_data(self,data):
+        print(status)
+
+
