087a91e6f8e8d385b922a8441b0edada909512e6
Pandey <amritanshu.pandey@intel.com>
2019-11-30 12:58:34 +0530

    able to generate live twitter csv data

--- tweet_extractor.py
+++ tweet_extractor.py
@@ -1,19 +1,22 @@
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
-
-consumer_key=""
-consumer_secret=""
-access_token=""
-access_token_secret=""
+import pandas as pd
+import json
+consumer_key="9KSy30k47bFDOT2fKfXWZSzTZ"
+consumer_secret="lR645zIuPcNDVgp9h1RaszRkl0vPYKj4DXmZS0GxkvKkGgcIob"
+access_token="1200417373729148928-eHpoZwI5bduQ64MBtEq1GugcgWlcjK"
+access_token_secret="4vxwdwZQBQnDzrVzcUWxQBB8hN8EkISiDt7QhZDkUvooc"
 
     
-class StdOutListener():
+class StdOutListener(StreamListener):
     def on_data(self,data):
-        print(data)
-        return true
+        df=pd.read_json(data,orient='records', lines=True)
+        pandas_data=pd.DataFrame(df)
+        df.to_csv('live_stream_test.csv')
+        return True
     
-    def on_error(self,data):
+    def on_error(self,status):
         print(status)
 
 if __name__ =="__main__":
@@ -22,6 +25,8 @@ if __name__ =="__main__":
     auth.set_access_token(access_token,access_token_secret)
     
     stream=Stream(auth,listener)
-    stream.filter(track=['falcon','Qatar'])
+    stream.filter(track=['Qatar'])
+   
+
 
 
